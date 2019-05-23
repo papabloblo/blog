0e125e76bf4d452c440f8f8afb6a9c747decc623
Pablo Hidalgo <pablohigar@gmail.com>
2019-05-23 17:31:02 +0200

    Pruebas de gráficos

--- cis_ideologia/README.md
+++ cis_ideologia/README.md
@@ -7,6 +7,11 @@ En las **encuestas electorales realizadas por el CIS** se pregunta
 La **idea** es visualizar cuál es la **distribución igeológica de cada partido político** y del congreso de los diputados (ponderando la distribución ideológica de cada partido por el porcentaje de diputados).
 
 **Idea secundaria**: diferencia entre cómo ven todos los ciudadanos la ideología de cada partido frente a cómo la ven la de su propio partido.
+
 # Datos
 
 > src/data
+
+# Gráficos
+
+<img src="dataviz/distribucion_ideologia.png" width="60%">
--- cis_ideologia/src/02_dataviz/01_distribucion_ideologia.R
+++ cis_ideologia/src/02_dataviz/01_distribucion_ideologia.R
@@ -31,7 +31,7 @@ ideologia_media <- ideologia_media_anyo %>%
   arrange(-ideologia_media)
 
 ideologia %>% 
-  filter(anyo == "2019") %>% 
+  # filter(anyo == "2019") %>% 
   mutate(partido = factor(partido, levels = ideologia_media$partido)) %>% 
   group_by(partido) %>% 
   mutate(porc_contestadas = porc_contestadas/max(porc_contestadas, na.rm = TRUE)) %>% 
@@ -54,6 +54,118 @@ ideologia %>%
   labs(
     title = "Percepción de la ideología",
     x = "")
+
+
+ideologia %>% 
+  filter(partido == "Congreso", ideologia <= 10, anyo == 2016) %>% 
+  # mutate(partido = factor(partido, levels = ideologia_media$partido)) %>% 
+  # group_by(partido) %>% 
+  # mutate(porc_contestadas = porc_contestadas/max(porc_contestadas, na.rm = TRUE)) %>% 
+  ggplot(
+    aes(x = ideologia,
+        y = porc_contestadas)
+  ) +
+  # geom_line(aes(group = anyo))+
+  geom_col(data = ideologia %>% 
+             filter(partido == "Congreso", ideologia <= 10, anyo == 2019),
+           aes(fill = partido), width = 1, fill = "firebrick") +
+  # geom_col(
+  #          aes(fill = partido), width = 1, alpha = 0.3) +
+  geom_segment(aes(x = ideologia - 0.5, xend = ideologia + 0.5,
+                   y = porc_contestadas, yend = porc_contestadas),
+               size = 2) +
+  geom_vline(xintercept = 5.5, size = 2) +
+  scale_x_continuous(breaks = 1:10) +
+  # facet_wrap(.~factor(partido, levels = orden_partidos), ncol = 1, scales = "free_y") +
+  # facet_wrap(.~anyo,  scales = "free_y", ncol = 1) +
+  tema +
+  theme(
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    panel.grid  = element_blank()
+  ) +
+  scale_fill_manual(values = color_partidos) +
+  labs(
+    title = "Percepción de la ideología",
+    x = "")
+
+partidos_selec <- c("PP", "PSOE", "Ciudadanos", "VOX", "Podemos", "Congreso")
+p <- ideologia %>% 
+  filter(partido %in% partidos_selec, 
+         ideologia <= 10, anyo == 2016) %>% 
+  mutate(partido = factor(partido, levels = ideologia_media$partido)) %>% 
+  # group_by(partido) %>% 
+  # mutate(porc_contestadas = porc_contestadas/max(porc_contestadas, na.rm = TRUE)) %>% 
+  ggplot(
+    aes(x = ideologia,
+        y = porc_contestadas)
+  ) +
+  # geom_line(aes(group = anyo))+
+  geom_col(data = ideologia %>% 
+             filter(partido %in% partidos_selec,
+                    ideologia <= 10, anyo == 2019) %>% 
+             mutate(partido = factor(partido, levels = ideologia_media$partido)),
+           aes(fill = partido), width = 1) +
+  # geom_col(
+  #          aes(fill = partido), width = 1, alpha = 0.3) +
+  geom_segment(aes(x = ideologia - 0.5, xend = ideologia + 0.5,
+                   y = porc_contestadas, yend = porc_contestadas),
+               size = 1) +
+  geom_vline(xintercept = 5.5, size = 1.5) +
+  scale_x_continuous(breaks = 1:10, 
+                     labels = function(x) case_when(
+                       x == 1 ~ paste(x, "\n(izquierda)"),
+                       x == 10 ~ paste(x, "\n(derecha)"),
+                       TRUE ~ as.character(x)
+                     )) +
+  # facet_wrap(.~factor(partido, levels = orden_partidos), ncol = 1, scales = "free_y") +
+  facet_wrap(.~partido,  
+             scales = "free_y",
+             ncol = 1) +
+  tema +
+  theme(
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    panel.grid  = element_blank()
+  ) +
+  scale_fill_manual(values = color_partidos) +
+  labs(
+    title = "Percepción de la ideología",
+   
+    caption = "Fuente: CIS\n\n@papabloblog",
+    y       = "",
+    x       = "Escala ideológica"
+  )
+
+ggsave("cis_ideologia/dataviz/ditribucion_ideologia.png", plot = p, dpi = 400, width = 20, height = 30, units = "cm")
+
+ideologia %>% 
+  filter(partido == "Congreso", ideologia <= 10, anyo >= 2016) %>% 
+  # mutate(partido = factor(partido, levels = ideologia_media$partido)) %>% 
+  # group_by(partido) %>% 
+  # mutate(porc_contestadas = porc_contestadas/max(porc_contestadas, na.rm = TRUE)) %>% 
+  ggplot(
+    aes(x = ideologia,
+        y = porc_contestadas)
+  ) +
+  geom_line(aes(group = anyo))+
+  # geom_col(aes(fill = partido), width = 1) +
+  geom_vline(xintercept = 5.5, size = 2) +
+  scale_x_continuous(breaks = 1:10) +
+  # facet_wrap(.~factor(partido, levels = orden_partidos), ncol = 1, scales = "free_y") +
+  # facet_wrap(.~anyo,  scales = "free_y", ncol = 1) +
+  tema +
+  theme(
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    panel.grid  = element_blank()
+  ) +
+  scale_fill_manual(values = color_partidos) +
+  labs(
+    title = "Percepción de la ideología",
+    x = "")
+
+
 
 ideologia_partidos %>% 
   filter(partido %in% partido_selec,
